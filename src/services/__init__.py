9114534cb42eb69460e52c0c98fa60f7f3a5f7e2
Observon <erickoliveirarangel@gmail.com>
2026-03-31 17:45:46 -0300

    refactor(services): remove eager imports from package initializer

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,11 +1,5 @@
 """Business logic services."""
 
-from src.services.data_loader import DataLoaderService
-from src.services.analyzer import AnalyzerService
-from src.services.export import ExportService
-from src.services.batch import BatchService
-from src.services.scheduler import SchedulerService
-
 __all__ = [
     "DataLoaderService",
     "AnalyzerService",
